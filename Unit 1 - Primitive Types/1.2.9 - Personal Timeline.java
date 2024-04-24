67cbb442facf044bfc3c1fd9a1cf030a7ae0ea98
OrigamiMaster15 <167934019+OrigamiMaster15@users.noreply.github.com>
2024-04-23 19:49:26 -0500

    Create 1.2.9 - Personal Timeline.java

---
+++ Unit 1 - Primitive Types/1.2.9 - Personal Timeline.java
@@ -0,0 +1,32 @@
+public class Timeline
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Age     | MM/YY - Description");
+        System.out.println("5       | ??/?? - Learn to make first origami");
+        System.out.println("        |");
+        System.out.println("        |");
+        System.out.println("        |");
+        System.out.println("7       |");
+        System.out.println("        |");
+        System.out.println("        |");
+        System.out.println("        |");
+        System.out.println("9       |");
+        System.out.println("        |");
+        System.out.println("        |");
+        System.out.println("        |");
+        System.out.println("11      |");
+        System.out.println("        |");
+        System.out.println("        |");
+        System.out.println("        |");
+        System.out.println("13      |");
+        System.out.println("        | 07/20 - Acquire first cat ~Panda");
+        System.out.println("        |");
+        System.out.println("        |");
+        System.out.println("15      | 06/21 - Acquire second cat ~Gyro");
+        System.out.println("        | 09/22 - Start new school friendships.");
+        System.out.println("        | 07/23 - Acquire third cat ~Nero");
+        System.out.println("        | 08/23 - Go back to school/strengthen friendly relationships.");
+        System.out.println("17      | 12/23 - Birthday");
+    }
+}
