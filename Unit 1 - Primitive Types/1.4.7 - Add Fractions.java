02b0a734f344b5ef45e49d92baf830cc2fce83dd
OrigamiMaster15 <167934019+OrigamiMaster15@users.noreply.github.com>
2024-04-25 18:29:07 -0500

    Create 1.4.7 - Add Fractions.java

---
+++ Unit 1 - Primitive Types/1.4.7 - Add Fractions.java
@@ -0,0 +1,17 @@
+public class AddFractions 
+{
+    public static void main(String[] args)
+    {
+        //Your code goes here!
+        int num1 = 1;
+        int den1 = 2;
+        int num2 = 2;
+        int den2 = 5;
+        
+        System.out.println("The numerator of the first fraction is " + num1);
+        System.out.println("The denominator of the first fraction is " + den1);
+        System.out.println("The numerator of the second fraction is " + num2);
+        System.out.println("The denominator of the second fraction is " + den2);
+        System.out.println("The sum of 1/2 + 2/5 = 9/10");
+    }
+}
