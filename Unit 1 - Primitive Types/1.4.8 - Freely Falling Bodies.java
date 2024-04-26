bc5c3c9da072077610475e75becce259e68cff08
OrigamiMaster15 <167934019+OrigamiMaster15@users.noreply.github.com>
2024-04-25 19:22:12 -0500

    Create 1.4.8 - Freely Falling Bodies.java

---
+++ Unit 1 - Primitive Types/1.4.8 - Freely Falling Bodies.java
@@ -0,0 +1,12 @@
+public class FallingBodies
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        double g = 9.8;
+        int t = 10;
+        
+        System.out.println("The height of the ball is " + .5*g*t*t + " m");
+        System.out.println("The velocity of the ball is " + g*t + " m/s");
+    }
+}
