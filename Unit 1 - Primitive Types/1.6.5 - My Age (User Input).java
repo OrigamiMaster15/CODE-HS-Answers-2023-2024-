c35bd7a8a3f4a1d5ebe3ee9da17e058667a58ffd
OrigamiMaster15 <167934019+OrigamiMaster15@users.noreply.github.com>
2024-04-25 19:25:47 -0500

    Create 1.6.5 - My Age (User Input).java

---
+++ Unit 1 - Primitive Types/1.6.5 - My Age (User Input).java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class MyAge
+{
+    public static void main(String[] args)
+    {
+        int age = 10;
+        
+        System.out.println("My current age is: " + age);
+        
+        age++;
+        
+        System.out.println("My age next year will be: " + age);
+        
+        age--;
+        
+        System.out.println("My current age is: " + age);
+        
+        age += 10;
+        
+        System.out.println("My current age is: " + age);
+        
+        age++;
+        
+        System.out.println("My age next year will be: " + age);
+        
+        age--;
+        
+        System.out.println("My current age is: " + age);
+        
+        age += 10;
+        
+        System.out.println("My current age is: " + age);
+        
+        age++;
+        
+        System.out.println("My age next year will be: " + age);
+        
+        age--;
+        
+        System.out.println("My current age is: " + age);
+    }
+}
