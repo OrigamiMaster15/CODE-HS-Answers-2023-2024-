5c11f0736bc0fb10f187f2a299808b01942ca8cd
OrigamiMaster15 <167934019+OrigamiMaster15@users.noreply.github.com>
2024-04-25 19:24:02 -0500

    Create 1.5.6 - My Age.java

---
+++ Unit 1 - Primitive Types/1.5.6 - My Age.java
@@ -0,0 +1,18 @@
+public class MyAge
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        int age = 16;
+        
+        System.out.println("My current age is: " + age);
+        
+        age++;
+        
+        System.out.println("My age next year is: " + age);
+        
+        age--;
+        
+        System.out.println("My current age is: " + age);
+    }
+}
